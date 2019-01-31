ecbeef50075f6a45ccb5ad0f960de4dea9deb07e
gmartin [gitflow branch] <gmartin@weborama.com>
2019-01-31 14:00:13 +0100

    rename assets

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -1,63 +1,75 @@
 <template>
   <div class="hello">
-      <img class="img-accueil img-accueil-1" :src="require('@/assets/oeuvre/memoire/gravure/Alerte 1939.jpg')" />
-      <img class="img-accueil img-accueil-2" :src="require('@/assets/oeuvre/portraits/peinture/Anne à l\'écharpe rouge.jpg')" />
-      <img class="img-accueil img-accueil-3" :src="require('@/assets/oeuvre/natures/peinture/Mangues et grenades.jpg')" />
-      <img class="img-accueil img-accueil-4" :src="require('@/assets/oeuvre/paysages/peinture/Ciel blanc sur Abu Tor, Jérusalem.jpg')" />
+    <img
+      class="img-accueil img-accueil-1"
+      :src="require('@/assets/oeuvre/memoire/gravure/Alerte_1939.jpg')"
+    />
+    <img
+      class="img-accueil img-accueil-2"
+      :src="require('@/assets/oeuvre/portraits/peinture/Anne_à_l\'écharpe_rouge.jpg')"
+    />
+    <img
+      class="img-accueil img-accueil-3"
+      :src="require('@/assets/oeuvre/natures/peinture/Mangues_et_grenades.jpg')"
+    />
+    <img
+      class="img-accueil img-accueil-4"
+      :src="require('@/assets/oeuvre/paysages/peinture/Ciel_blanc_sur_Abu_Tor,_Jérusalem.jpg')"
+    />
   </div>
 </template>
 
 <script>
 export default {
-    props: ['cat', 'type'],
-    name: 'Oeuvre',
-    data() {
-        return {
-            msg: 'Welcome to Your Vue.js App'
-        }
-    }
-}
+  props: ["cat", "type"],
+  name: "Oeuvre",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
 </script>
 
 <style >
 h1,
 h2 {
-    font-weight: normal;
+  font-weight: normal;
 }
 ul {
-    list-style-type: none;
-    padding: 0;
+  list-style-type: none;
+  padding: 0;
 }
 li {
-    display: inline-block;
-    margin: 0 10px;
+  display: inline-block;
+  margin: 0 10px;
 }
 a {
-    color: #42b983;
+  color: #42b983;
 }
 
 .img-accueil {
-    max-width: 40%;
+  max-width: 40%;
 
-    position: absolute;
+  position: absolute;
 }
 
 .img-accueil-1 {
-    top: 15%;
-    max-height: 38vh;
+  top: 15%;
+  max-height: 38vh;
 }
 .img-accueil-2 {
-    top: 15%;
-    left: 55%;
-    max-height: 38vh;
+  top: 15%;
+  left: 55%;
+  max-height: 38vh;
 }
 .img-accueil-3 {
-    top: 60%;
-    max-height: 38vh;
+  top: 60%;
+  max-height: 38vh;
 }
 .img-accueil-4 {
-    top: 62%;
-    left: 55%;
-    max-height: 38vh;
+  top: 62%;
+  left: 55%;
+  max-height: 38vh;
 }
 </style>
